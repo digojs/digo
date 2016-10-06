adf6da5f0f82a2b1b817c54a15b0ea45e03ff1fe
xuld <xuld@vip.qq.com>
2016-10-06 21:59:49 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,79 @@
+digo
+==============================
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Coveralls Status][coveralls-image]][coveralls-url] [![Gitter chat][gitter-image]][gitter-url]
+
+digo 是一个轻量、实用的基于规则的自动化构建工具。
+
+digo 为项目提供了代码预编译、模块依赖打包、压缩优化等自动化构建功能。
+
+特性
+-------------------------------
+1. **轻量**：您只需简单声明构建的规则，立即就能享受自动化发布的快感。
+2. **高性能**：得益于 [Node.js](https://nodejs.org/) 的异步 IO 特性，digo 可以高效地构建项目。
+3. **易上手**: 只需三分钟看完[入门指南](https://github.com/digojs/digo/wiki/入门指南)，马上就能实现预期的效果。
+4. **易扩展**: 使用[插件](https://github.com/digojs/digo-plugins#digo-插件列表)，轻松满足不同的发布需求。
+
+下载安装
+-------------------------------
+```
+$ npm install digo -g
+```
+> 如果安装不成功，请[点击这里](https://github.com/digojs/digo/wiki/常见问题#安装失败)。
+
+文档
+-------------------------------
+- [入门指南](https://github.com/digojs/digo/wiki/入门指南)
+- [命令行文档](https://github.com/digojs/digo/wiki/命令行)
+- [API 文档](https://github.com/digojs/digo/wiki/API)
+- [插件列表](https://github.com/digojs/digo-plugins#digo-插件列表)
+- [插件开发指南](https://github.com/digojs/digo/wiki/编写插件)
+- [digo vs gulp & webpack](https://github.com/digojs/digo/wiki/产品比较)
+
+`digo.config.js` 示例
+-------------------------------
+```js
+var digo = require("digo");
+
+exports.default = function() {
+    digo.src("src/images/**/*").pipe("digo-imagemin").dest("_build/images");
+    digo.src("src/css/**/*.less").pipe("digo-less").pipe("digo-autoprefixer").dest("_build/css");
+    digo.src("src/js/**/*.js").pipe("digo-babel").dest("_build/js");
+    digo.src("src/**/*.html").pipe("digo-include").dest("_build");
+};
+
+exports.watch = function() {
+    digo.watch(exports.default);
+};
+
+exports.publish = function() {
+    exports.default();
+    digo.then(function() {
+        digo.src("_build/images/**/*").dest("_dist/images");
+        digo.src("_build/css/**/*.css").pipe("digo-cleancss").dest("_dist/css");
+        digo.src("_build/js/**/*.js").pipe("digo-uglify-js").dest("_dist/js");
+        digo.src("_build/**/*.html").pipe("digo-html-minifier").dest("_dist");
+    });
+};
+```
+
+贡献代码
+-------------------------------
+社区支持始终是国内开源项目的硬伤。
+
+我们忠心地希望得到您的支持，如果您觉得这个项目不错，请点击右上角的关注。
+或者您还可以:
+- [报告 BUG](https://github.com/digo/digo/issues/new)
+- [提交需求、建议](https://github.com/digo/digo/issues/new)。
+- [共享一个插件](https://github.com/digojs/digo/wiki/编写插件)
+- [贡献代码](https://github.com/digojs/digo/wiki/贡献代码)
+
+[npm-url]: https://www.npmjs.com/package/digo
+[npm-image]: https://img.shields.io/npm/v/digo.svg
+[downloads-image]: https://img.shields.io/npm/dm/digo.svg
+[downloads-url]: http://badge.fury.io/js/digo
+[travis-url]: https://travis-ci.org/digojs/digo
+[travis-image]: https://img.shields.io/travis/digojs/digo.svg
+[coveralls-url]: https://coveralls.io/github/digojs/digo
+[coveralls-image]: https://img.shields.io/coveralls/digojs/digo/master.svg
+[gitter-url]: https://gitter.im/digojs/digo
+[gitter-image]: https://img.shields.io/badge/gitter-digo%2Fdigo-brightgreen.svg
---
+++ digo.config.js
@@ -0,0 +1,127 @@
+
+exports.default = exports.build = function () {
+    exports.compile() && exports.test();
+};
+
+exports.compile = function () {
+    return exec("node ./node_modules/typescript/bin/tsc -p tsconfig.json") === 0;
+};
+
+exports.watch = function () {
+    return exec("node ./node_modules/typescript/bin/tsc -p tsconfig.json -w") === 0;
+};
+
+exports.test = function () {
+    var file = (process.argv[3] || "").substr(process.cwd().length + 1)
+        .replace(/\\/g, "/")
+        .replace(/^lib\/(.*)\.ts$/, "_build/test/$1Test.js")
+        .replace(/^test\/(.*)\.ts$/, "_build/test/$1.js");
+    if (!/^_build\/test\//.test(file)) {
+        file = "_build/test/**/*Test.js";
+    }
+
+    process.argv.length = 2;
+    process.argv.push(file, "--no-timeouts", "--colors", "--ui", "exports");
+
+    require("source-map-support/register");
+    require("mocha/bin/_mocha");
+};
+
+exports.coverage = function () {
+    return exec("node ./node_modules/istanbul/lib/cli.js cover --dir _coverage node_modules/mocha/bin/_mocha _build/test/**/*Test.js -- --ui exports") === 0;
+};
+
+exports.coveralls = function () {
+    return exec("node ./node_modules/istanbul/lib/cli.js cover --report lcovonly --dir _coverage node_modules/mocha/bin/_mocha _build/test/**/*Test.js -- --ui exports") === 0 &&
+        exec("node ./node_modules/coveralls/bin/coveralls.js < ./_coverage/lcov.info") === 0 &&
+        del("_coverage");
+};
+
+exports.publish = function () {
+
+    del("_dist");
+
+    var tsconfig = require("./tsconfig");
+    tsconfig.compilerOptions.sourceMap = false;
+    tsconfig.compilerOptions.outDir = "_dist";
+    tsconfig.exclude.push("test");
+
+    var fs = require("fs");
+    try {
+        fs.mkdirSync("_dist");
+    } catch (e) { }
+    fs.writeFileSync("_dist/tsconfig.json", JSON.stringify(tsconfig));
+    try {
+        if (exec("node ./node_modules/typescript/bin/tsc -p _dist/tsconfig.json") !== 0) {
+            return;
+        }
+    } finally {
+        fs.unlinkSync("_dist/tsconfig.json");
+    }
+
+    var package = require("./package.json");
+    package.version = package.version.replace(/(\d+\.\d+\.)(\d+)/, function (_, prefix, postfix) {
+        return prefix + (+postfix + 1);
+    });
+    fs.writeFileSync("package.json", JSON.stringify(package, null, 2));
+    package.main = package.main.replace("_build/", "");
+    package.typings = package.typings.replace(".ts", ".d.ts");
+    package.bin.digo = package.bin.digo.replace("_build/", "");
+    delete package.scripts;
+    fs.writeFileSync("_dist/package.json", JSON.stringify(package, null, 2));
+
+    copy("README.md", "_dist/README.md");
+    copy("LICENSE", "_dist/LICENSE");
+
+    try {
+        require("./dist/");
+    } catch (e) {
+        return;
+    }
+
+};
+
+exports.clean = function () {
+    del("_build");
+    del("_coverage");
+    del("_dist");
+};
+
+function exec(command, options) {
+    try {
+        require("child_process").execSync(command, options);
+    } catch (e) {
+        if (e.stdout && e.stdout.length) {
+            console.log(e.stdout.toString().trim());
+        }
+        if (e.stderr && e.stderr.length) {
+            console.error(e.stderr.toString().trim());
+        }
+        return e.status;
+    }
+    return 0;
+}
+
+function copy(from, to) {
+    var fs = require("fs");
+    fs.writeFileSync(to, fs.readFileSync(from));
+}
+
+function del(dir) {
+    var fs = require("fs");
+    try {
+        fs.unlinkSync(path);
+    } catch (e) { }
+    try {
+        fs.readdirSync(path).forEach(name => {
+            del(path + "/" + name);
+        });
+    } catch (e) { }
+    try {
+        fs.rmdirSync(path);
+    } catch (e) { }
+}
+
+if (process.mainModule === module) {
+    exports[process.argv[2] || "default"]();
+}
