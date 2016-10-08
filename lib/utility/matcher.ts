67a8c7f70bdea8613b182365aee3d5389a0ad25d
xuld <xuld@vip.qq.com>
2016-10-08 17:54:58 +0800

    匹配器

---
+++ lib/utility/matcher.ts
@@ -0,0 +1,265 @@
+/**
+ * @fileOverview 匹配器
+ * @author xuld <xuld@vip.qq.com>
+ */
+import * as np from "path";
+import { commonDir, relativePath } from "./path";
+
+/**
+ * 表示一个匹配器。
+ */
+export class Matcher {
+
+    /**
+     * 获取所有已编译的模式列表。
+     */
+    compiledPatterns: CompiledPattern[] = [];
+
+    /**
+     * 获取当前匹配器的忽略匹配器。如果不存在则返回 undefined。
+     */
+    ignoreMatcher: Matcher;
+
+    /**
+     * 初始化新的匹配器。
+     * @param pattern 要添加的匹配模式。
+     * @param cwd 所有路径的基路径。
+     */
+    constructor(pattern?: Pattern, cwd?: string) {
+        pattern && this.add(pattern, cwd);
+    }
+
+    /**
+     * 添加一个匹配模式。
+     * @param pattern 要添加的匹配模式。
+     * @param cwd 所有路径的基路径。
+     */
+    add(pattern: Pattern, cwd?: string) {
+        if (typeof pattern === "string") {
+            if (pattern.charCodeAt(0) === 33/*!*/) {
+                (this.ignoreMatcher || (this.ignoreMatcher = new Matcher)).compiledPatterns.push(globToRegExp(pattern.substr(1), cwd));
+            } else {
+                this.compiledPatterns.push(globToRegExp(pattern, cwd));
+            }
+        } else if (Array.isArray(pattern)) {
+            for (const p of pattern) {
+                this.add(p, cwd);
+            }
+        } else if (pattern instanceof RegExp) {
+            this.compiledPatterns.push({
+                base: resolveBase(cwd),
+                test(path) {
+                    return (<RegExp>pattern).test(relativePath(this.base, path));
+                }
+            });
+        } else if (typeof pattern === "function") {
+            this.compiledPatterns.push({
+                base: resolveBase(cwd),
+                test: pattern
+            });
+        } else if (pattern instanceof Matcher) {
+            this.compiledPatterns.push(...pattern.compiledPatterns);
+            if (pattern.ignoreMatcher) {
+                (this.ignoreMatcher || (this.ignoreMatcher = new Matcher)).add(pattern.ignoreMatcher);
+            }
+        }
+        return this;
+    }
+
+    /**
+     * 添加一个忽略模式。
+     * @param pattern 要添加的模式。
+     */
+    addIgnore(pattern: Pattern) {
+        (this.ignoreMatcher || (this.ignoreMatcher = new Matcher)).add(pattern);
+        return this;
+    }
+
+    /**
+     * 测试指定的绝对路径是否匹配。
+     * @param path 要测试的绝对路径。
+     * @returns 如果匹配任意一个已添加的模式且未被忽略则返回 true，否则返回 false。
+     */
+    match(path: string): boolean {
+        end: if (this.compiledPatterns.length) {
+            for (const pattern of this.compiledPatterns) {
+                if (pattern.test(path)) {
+                    break end;
+                }
+            }
+            return false;
+        }
+        return !this.ignoreMatcher || !this.ignoreMatcher.match(path);
+    }
+
+    /**
+     * 获取所有模式的公共基路径。
+     * @returns 返回基路径部分(末尾含分隔符)。如果没有相同部分则返回空字符串。
+     */
+    get base() {
+        let result: string;
+        for (const compiledPattern of this.compiledPatterns) {
+            result = result == undefined ? compiledPattern.base : commonDir(result + "/_", compiledPattern.base + "/_");
+        }
+        return result;
+    }
+
+}
+
+/**
+ * 表示一个通配符、正则表达式、测试函数或以上模式组成的数组。
+ * @remark
+ * ##### 通配符
+ * 通配符的语法和 [`.gitignore`](https://git-scm.com/docs/gitignore) 相同，
+ * 支持以下特殊字符：
+ * - `*`: 匹配任意个字符（/ 除外)。
+ * - `**`:匹配任意个字符。
+ * - `?`: 匹配一个字符(/ 除外)。
+ * - `\`: 表示转义字符。
+ * - `[abc]`: 匹配括号中的任一个字符。
+ * - `[^abc]`: 匹配括号中的任一个字符以外的字符。
+ * 
+ * 在通配符前面加 `!`，表示忽略匹配的项。
+ * 注意如果忽略了父文件夹，出于性能考虑，无法重新包含其中的子文件。
+ * 
+ * 如果通配符以 `/` 结尾，表示只匹配文件夹。
+ * 
+ * 关于 `*` 和 `**` 的区别：
+ * - `*` 只匹配一层目录。比如 `usr/*\/foo.js` 匹配 `usr/dir/foo.js`，但不匹配 `usr/foo.js`，也不匹配 `usr/dir/sub/foo.js`。
+ * - `**` 匹配任意层目录(包括没有)。比如 `usr/**\/foo.js` 既匹配 `usr/dir/foo.js`，也匹配 `usr/foo.js` 和 `usr/dir/sub/foo.js`。
+ * - 特殊情况：如果通配符不含 /(末尾除外)，则两者意义相同：都表示匹配任意层目录。比如 `*.js` 既匹配 `foo.js` 也匹配 `usr/foo.js`。如果希望只匹配根目录下的文件，应该写成 `/*.js`。
+ *
+ * ##### 正则表达式
+ * 复杂的匹配规则可以使用正则表达式。
+ * 测试的源是一个固定以 / 为分隔符的相对路径。
+ *
+ * ##### 自定义函数
+ * 函数接收一个绝对路径为参数，如果函数返回 true 表示匹配该路径。
+ * ```js
+ * function match(path) {
+ *     return path.indexOf("abc/") >= 0;
+ * }
+ * ```
+ */
+export type Pattern = string | RegExp | ((path: string) => boolean) | any[] | Matcher;
+
+/**
+ * 表示一个已编译的模式。
+ */
+export interface CompiledPattern {
+
+    /**
+     * 基路径(末尾含分隔符)。
+     */
+    base: string;
+
+    /**
+     * 测试是否匹配指定的路径。
+     * @param path 要测试的绝对路径。
+     * @returns 如果匹配则返回 true，否则返回 false。
+     */
+    test(path: string): boolean;
+
+}
+
+const compiledPatterns: { [pattern: string]: CompiledPattern & { cwd: string }; } = { __proto__: null };
+
+/**
+ * 将指定的通配符转为等效的正则表达式。
+ * @param pattern 要处理的通配符。
+ * @param cwd 所有路径的基路径。
+ * @return 返回正则表达式。
+ */
+function globToRegExp(pattern: string, cwd: string) {
+    const cache = compiledPatterns[pattern];
+    if (cache && cache.cwd === cwd) {
+        return cache;
+    }
+
+    let glob = np.posix.normalize(pattern);
+    let root = resolveBase(cwd);
+
+    // 提取通配符的基路径。
+    let base: string;
+    const firstGlob = glob.search(/[*?\\]|\[.+\]/);
+    const match = /^\/?((?:[^\/]+\/)+)/.exec(firstGlob >= 0 ? glob.substr(0, firstGlob) : glob);
+    if (match) {
+        root = np.join(root, match[1]);
+        base = firstGlob >= 0 ? root : np.join(root, glob.substr(match[0].length));
+        glob = glob.substr(match[0].length - 1); // 保留通配符的 / 前缀以便后续正确处理 /。
+    } else {
+        base = root;
+    }
+
+    // 剩余部分转为正则表达式。
+    let hasSlash: boolean;
+    let hasSlashPostfix: boolean;
+    let regex = glob.replace(/\\.|\[.+\]|\*\*\/?|[*?\-+.^|\\{}()[\]/]/g, (all: string, index: number) => {
+        switch (all.charCodeAt(0)) {
+            case 47/*/*/:
+                if (index === 0) {
+                    hasSlash = true;
+                    if (index === glob.length - 1) {
+                        hasSlashPostfix = true;
+                    }
+                    return "";
+                }
+                if (index === glob.length - 1) {
+                    hasSlashPostfix = true;
+                } else {
+                    hasSlash = true;
+                }
+                return sep;
+            case 42/***/:
+                return all.length > 2 ? `(.*${sep})?` : all.length > 1 ? "(.*)" : `([^${sep}]*)`;
+            case 63/*?*/:
+                return `([^${sep}])`;
+            case 92/*\*/:
+                return index === pattern.length - 1 ? " " : escapeRegExp(all.charAt(1));
+            case 91/*[*/:
+                if (all.length > 2) {
+                    index = all.charCodeAt(1) === 94/*^*/ ? 2 : 1;
+                    return (index === 1 ? '[' : '[^') + escapeRegExp(all.substring(index, all.length - 1)) + ']';
+                }
+            // fall through
+            default:
+                return "\\" + all;
+        }
+    });
+
+    // 如果不存在 /(末尾除外)，则允许匹配任意位置。
+    if (hasSlash) {
+        regex = "^" + escapeRegExp(root) + regex;
+    }
+
+    // 如果末尾不存在 /，则必须匹配到结尾或文件夹结尾。
+    if (!hasSlashPostfix) regex += `(?:$|${sep})`;
+
+    // 生成正则表达式。
+    const result: CompiledPattern & { cwd: string } = compiledPatterns[pattern] = <any>new RegExp(regex, np.sep === "\\" ? "i" : "");
+    result.cwd = cwd;
+    result.base = base;
+    return result;
+}
+
+const sep = escapeRegExp(np.sep);
+
+/**
+ * 编码字符串里的正则表达式特殊字符。
+ * @param pattern 要处理的通配符。
+ * @return 返回处理后的模式字符串。
+ */
+function escapeRegExp(pattern: string) {
+    return pattern.replace(/[-+.^$?*|\\{}()[\]]/g, "\\$&");
+}
+
+/**
+ * 解析基路径。
+ * @param path 要解析的路径。
+ * @return 返回已解析的基路径(含分隔符)。
+ */
+function resolveBase(path: string) {
+    path = np.resolve(path || "");
+    if (!path.endsWith(np.sep)) path += np.sep;
+    return path;
+}
---
+++ test/utility/matcherTest.ts
@@ -0,0 +1,95 @@
+﻿import * as assert from "assert";
+import * as np from "path";
+import * as matcher from "../../lib/utility/matcher";
+
+export namespace matcherTest {
+
+    export function matchTest() {
+        assert.equal(match(null, 'foo.js'), true);
+        assert.equal(match("foo", "foo"), true);
+        assert.equal(match("foo.js", "foo"), false);
+        assert.equal(match("foo/goo", "foo/goo2"), false);
+        assert.equal(match("foo.js", "foo.js"), true);
+        assert.equal(match('path/to/*.js', 'path/to/foo.js'), true);
+        assert.equal(match('path/to/*.js', 'path/to/foo.css'), false);
+        assert.equal(match('path/to/*.js', 'path/to.js'), false);
+        assert.equal(match('path/to/*.js', 'path/to/foo/goo.js'), false);
+        assert.equal(match('/root/path/to/*.js', 'root/path/to/foo.js'), true);
+        assert.equal(match('/root/path/to/*.js', 'root/path/toa/foo.js'), false);
+        assert.equal(match('/*.js', 'foo.js'), true);
+        assert.equal(match('/*.js', 'foo/goo.js'), false);
+        assert.equal(match('*.js', 'foo.js'), true);
+        assert.equal(match('*.js', 'goo/foo.js'), true);
+        assert.equal(match('*.js', 'foo/goo/.js'), true);
+        assert.equal(match('**/*.js', 'foo/goo/.js'), true);
+        assert.equal(match('**/*.js', '.js'), true);
+        assert.equal(match('path/?', 'path/a'), true);
+        assert.equal(match('path/?', 'path/ab'), false);
+        assert.equal(match('path/[ab]', 'path/a'), true);
+        assert.equal(match('path/[ab]', 'path/b'), true);
+        assert.equal(match('path/[ab]', 'path/ab'), false);
+        assert.equal(match('path/[^ab]', 'path/a'), false);
+        assert.equal(match('path/[^ab]', 'path/b'), false);
+        assert.equal(match('path/[^ab]', 'path/c'), true);
+        assert.equal(match('path/*', 'path/'), false);
+        assert.equal(match('path/*', 'path/foo'), true);
+        assert.equal(match('path/foo*', 'path/foo'), true);
+        assert.equal(match('path/a*', 'path/abcd'), true);
+        assert.equal(match('path/foo/', 'path/foo/goo'), true);
+        assert.equal(match('path/*/', 'path/foo/goo'), true);
+        assert.equal(match('path/*/', 'path/foo'), false);
+        assert.equal(match('path/*', 'path/foo'), true);
+        assert.equal(match('path/foo/', 'path/foo/'), false);
+        assert.equal(match('path/**/*', 'path/'), false);
+        assert.equal(match('path/**/*', 'path/foo'), true);
+        assert.equal(match('path/**/subdir/foo.*', 'path/subdir/foo.js'), true);
+        assert.equal(match('path/**/subdir/foo.*', 'path/foo/subdir/foo.js'), true);
+        assert.equal(match('path/**/subdir/foo.*', 'path/foo/subdir/foo1.js'), false);
+        assert.equal(match('path/**/subdir/foo.*', 'path/foo/subdir/foo'), false);
+        assert.equal(match('path/**/subdir/foo.*', 'path/foo/foo2/subdir/foo.txt'), true);
+        assert.equal(match('path/**/subdir/foo.*', 'path/foo/foo2/subdir/foo'), false);
+        assert.equal(match('./path/**/subdir/foo.*', 'path/foo/foo2/subdir/foo.txt'), true);
+        assert.equal(match('./path/**/subdir/foo.*', 'path/foo/foo2/subdir/foo.txt'), true);
+        assert.equal(match('../path/**/subdir/foo.*', '../path/foo/foo2/subdir/foo.txt'), true);
+
+        assert.equal(match(['../path/**/subdir/foo.*', "!foo.txt"], '../path/foo/foo2/subdir/Foo.txt'), false);
+        assert.equal(match(file => true, 'foo.js'), true);
+        assert.equal(match([file => true, "!foo.js"], 'foo.js'), false);
+        assert.equal(match(file => false, 'foo.js'), false);
+        assert.equal(match("\\foo.js", 'foo.js'), true);
+        assert.equal(match("\\*.js", 'foo.js'), false);
+        assert.equal(match("[.js", '[.js'), true);
+        assert.equal(match(null, 'foo.js'), true);
+
+        assert.equal(match(/foo\.js/, 'foo.js'), true);
+        assert.equal(match(new matcher.Matcher(/foo\.js/, "!goo.js"), 'goo.js'), false);
+        assert.equal(match(new matcher.Matcher(/foo\.js/).addIgnore("goo.js"), 'goo.js'), false);
+
+        function match(pattern: matcher.Pattern, source: string) {
+            return new matcher.Matcher(pattern).match(np.resolve(source));
+        }
+    }
+
+    export function baseTest() {
+        assert.equal(new matcher.Matcher("foo").base, np.resolve(".") + np.sep);
+        assert.equal(new matcher.Matcher('path/to/*.js').base, np.resolve('path/to') + np.sep);
+        assert.equal(new matcher.Matcher('/root/path/to/*.js').base, np.resolve('root/path/to') + np.sep);
+        assert.equal(new matcher.Matcher('/*.js').base, np.resolve('.') + np.sep);
+        assert.equal(new matcher.Matcher('*.js').base, np.resolve('.') + np.sep);
+        assert.equal(new matcher.Matcher('**/*.js').base, np.resolve('.') + np.sep);
+        assert.equal(new matcher.Matcher('path/?').base, np.resolve('path/') + np.sep);
+        assert.equal(new matcher.Matcher('path/foo[ab]').base, np.resolve('path/') + np.sep);
+        assert.equal(new matcher.Matcher('path/*').base, np.resolve('path/') + np.sep);
+        assert.equal(new matcher.Matcher('path/foo*').base, np.resolve('path/') + np.sep);
+        assert.equal(new matcher.Matcher('path/**/*').base, np.resolve('path/') + np.sep);
+        assert.equal(new matcher.Matcher('path/**/subdir/foo.*').base, np.resolve('path') + np.sep);
+        assert.equal(new matcher.Matcher("foo/").base, np.resolve("foo/") + np.sep);
+        assert.equal(new matcher.Matcher(["foo/goo", "foo/foo"]).base, np.resolve("foo") + np.sep);
+        assert.equal(new matcher.Matcher(["foo/**/*.js", "foo/**/*.css"]).base, np.resolve("foo") + np.sep);
+        assert.equal(new matcher.Matcher(/foo/).base, np.resolve("") + np.sep);
+        assert.equal(new matcher.Matcher(["foo/", "!foo/"]).base, np.resolve("foo") + np.sep);
+        assert.equal(new matcher.Matcher(["../foo/**/*.js", "foo/**/*.css"]).base, np.resolve("..") + np.sep);
+        assert.equal(new matcher.Matcher(["../foo/**/*.js", /foo/]).base, np.resolve("..") + np.sep);
+    }
+
+}
