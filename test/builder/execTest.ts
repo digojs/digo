475bf13f007489c2b808712131f4847710a4d369
xuld <xuld@vip.qq.com>
2016-10-09 20:03:27 +0800

    强制隐藏进度

--- test/builder/execTest.ts
+++ test/builder/execTest.ts
@@ -1,8 +1,18 @@
 import * as assert from "assert";
 import * as consoleHelper from "../helper/consoleHelper";
+import then = require("../../lib/builder/then");
 import exec = require("../../lib/builder/exec");
 
 export namespace execTest {
+
+    const colors = then.progress;
+    export function before() {
+        then.progress = false;
+    }
+
+    export function after() {
+        then.progress = colors;
+    }
 
     export function execTest(done: MochaDone) {
         consoleHelper.redirectOutput((outputs, cb) => {
--- test/builder/pluginTest.ts
+++ test/builder/pluginTest.ts
@@ -1,7 +1,17 @@
 import * as assert from "assert";
+import then = require("../../lib/builder/then");
 import plugin = require("../../lib/builder/plugin");
 
 export namespace pluginTest {
+
+    const colors = then.progress;
+    export function before() {
+        then.progress = false;
+    }
+
+    export function after() {
+        then.progress = colors;
+    }
 
     export function pluginTest() {
         assert.equal(plugin.plugin("typescript"), require("typescript"));
