02501cfa1784e55eafba42fd9def74962471fe83
xuld <xuld@vip.qq.com>
2016-10-24 12:38:52 +0800

    改进 pipe 提示

--- lib/builder/fileList.ts
+++ lib/builder/fileList.ts
@@ -123,7 +123,7 @@ export class FileList extends EventEmitter {
     }
 
     /**
-     * 存储当前列表正在等待的子列表。
+     * 存储当前列表正在等待的直接子列表。
      */
     private activeList: FileList;
 
@@ -131,10 +131,9 @@ export class FileList extends EventEmitter {
      * 将所有文件传递给目标文件列表或处理器。
      * @param processor 目标文件列表或处理器。
      * @param options 传递给处理器的只读配置对象。
-     * @param load 是否在处理前载入文件内容。
      * @return 返回新的文件列表。
      */
-    pipe<T>(processor: Processor<T>, options?: T, load?: boolean): FileList {
+    pipe<T>(processor: string | FileList | SimpleProcessor<T> | Processor<T>, options?: T): FileList {
 
         // .pipe("..."): 支持直接写插件名。
         while (typeof processor === "string") {
@@ -148,16 +147,38 @@ export class FileList extends EventEmitter {
             return processor;
         }
 
+        // .pipe(file => ...): 转为标准处理器。
+        if (typeof processor === "function") {
+            processor = processor.length < 3 ? {
+                name: processor.name,
+                load: true,
+                eachSync: <any>processor
+            } : {
+                    name: processor.name,
+                    load: true,
+                    each: processor
+                };
+        }
+
         // 载入文件内容。
-        if (load !== false && processor.load !== false) {
-            return this.pipe((file, options, done) => {
-                file.load((error, file) => {
-                    if (error) {
-                        file.error(error);
-                    }
-                    done();
-                });
-            }, options, false).pipe(processor, options, false);
+        if (processor.load !== false) {
+            return this.pipe({
+                load: false,
+                each(file, options, done) {
+                    file.load((error, file) => {
+                        if (error) {
+                            file.error(error);
+                        }
+                        done();
+                    });
+                }
+            }).pipe({
+                load: false,
+                name: processor.name,
+                each: processor.each,
+                eachSync: processor.eachSync,
+                transform: processor.transform
+            }, options);
         }
 
         // 补齐默认参数。
@@ -165,68 +186,78 @@ export class FileList extends EventEmitter {
             options = <T>defaultProcessorOptions;
         }
 
-        // 查询当前激活的子列表。
-        let active = this.activeList;
-        if (active) {
-            while (active.activeList) {
-                active = active.activeList;
+        // 获取等待列表。
+        let currentActive = this.activeList;
+        if (currentActive) {
+            while (currentActive.activeList) {
+                currentActive = currentActive.activeList;
             }
         }
 
         // 创建结果列表。
-        const result = this.activeList = new FileList(this.asyncQueue);
+        const destList = this.activeList = new FileList(this.asyncQueue);
 
-        // .pipe(file => ...)
-        if (typeof processor === "function") {
+        // .pipe({transform}): 自定义转换。
+        if (processor.transform) {
+            if (currentActive) {
+                currentActive.on("end", () => {
+                    (<Processor<T>>processor).transform(this, destList, options);
+                });
+            } else {
+                processor.transform(this, destList, options);
+            }
+        }
 
+        // .pipe({each}): 遍历处理。
+        if (!processor.transform || processor.each || processor.eachSync) {
             let pending = 1; // 需要等待所有 data 事件 + 1 个 end 事件。
-
             const onData = (file: File) => {
                 pending++;
-                const taskId = (<Function>processor).name && begin("{default:processor}: {file}", {
-                    processor: (<Function>processor).name,
+                const taskId = (<Processor<T>>processor).name && begin("{default:processor}: {file}", {
+                    processor: (<Processor<T>>processor).name,
                     file: file.toString()
                 });
                 function done() {
                     taskId && end(taskId);
-                    result.add(file);
+                    destList.add(file);
                     onEnd();
                 }
-                if ((<Function>processor).length < 3) {
+                if ((<Processor<T>>processor).each) {
                     try {
-                        (<Function>processor)(file, options, null, this, result);
+                        (<Processor<T>>processor).each(file, options, done, this, destList);
                     } catch (e) {
                         file.error({
-                            plugin: (<Function>processor).name,
-                            error: e
-                        });
-                    }
-                    done();
-                } else {
-                    try {
-                        (<Function>processor)(file, options, done, this, result);
-                    } catch (e) {
-                        file.error({
-                            plugin: (<Function>processor).name,
+                            plugin: (<Processor<T>>processor).name,
                             error: e
                         });
                         done();
                     }
+                } else {
+                    if ((<Processor<T>>processor).eachSync) {
+                        try {
+                            (<Processor<T>>processor).eachSync(file, options, this, destList);
+                        } catch (e) {
+                            file.error({
+                                plugin: (<Processor<T>>processor).name,
+                                error: e
+                            });
+                        }
+                    }
+                    done();
                 }
             };
 
             const onEnd = () => {
                 if (--pending > 0) return;
-                result.end();
+                destList.end();
             };
 
-            if (active) {
-                // active 存储了上一次 pipe 的结果列表。
-                // 实际当前操作是 pipe 到 active 的。
+            if (currentActive) {
+                // a.pipe(A); a.pipe(B) => a.pipe(A).pipe(B); 
                 this.on("end", files => {
                     const all = files.slice();
                     let left = all.length;
-                    active.on("data", file => {
+                    currentActive.on("data", file => {
                         const p = all.indexOf(file);
                         if (p >= 0 && all[p]) {
                             all[p] = null;
@@ -238,7 +269,7 @@ export class FileList extends EventEmitter {
                             }
                         }
                     });
-                    active.on("end", () => {
+                    currentActive.on("end", () => {
                         // 如果未剩余文件，则表示当前列表已处理完成。
                         if (left === 0) return;
                         for (const file of all) {
@@ -253,20 +284,9 @@ export class FileList extends EventEmitter {
                 this.on("data", onData);
                 this.on("end", onEnd);
             }
-        } else if ((<{ transform: Function }>processor).transform) {
-            // .pipe({...})
-            if (active) {
-                active.on("end", () => {
-                    (<{ transform: Function }>processor).transform(this, result, options);
-                });
-            } else {
-                (<{ transform: Function }>processor).transform(this, result, options);
-            }
-        } else {
-            throw new TypeError("pipe(): Invalid processor");
         }
 
-        return result;
+        return destList;
     }
 
     /**
@@ -274,14 +294,17 @@ export class FileList extends EventEmitter {
      * @param dir 要保存的目标文件文件夹。如果为空则保存到原文件夹。
      */
     dest(dir?: string | ((file: File) => string)) {
-        return this.pipe((file, dir, done) => {
-            file.save(typeof dir === "function" ? dir(file) : dir, (error, file) => {
-                if (error) {
-                    file.error(error);
-                }
-                done();
-            });
-        }, dir, false);
+        return this.pipe({
+            load: false,
+            each(file, dir, done) {
+                file.save(typeof dir === "function" ? dir(file) : dir, (error, file) => {
+                    if (error) {
+                        file.error(error);
+                    }
+                    done();
+                });
+            }
+        }, dir);
     }
 
     /**
@@ -289,14 +312,17 @@ export class FileList extends EventEmitter {
      * @param deleteDir 指示是否删除空的父文件夹。默认为 true。
      */
     delete(deleteDir?: boolean) {
-        return this.pipe((file, deleteDir, done) => {
-            file.delete(deleteDir, (error, file) => {
-                if (error) {
-                    file.error(error);
-                }
-                done();
-            });
-        }, deleteDir, false);
+        return this.pipe({
+            load: false,
+            each(file, deleteDir, done) {
+                file.delete(deleteDir, (error, file) => {
+                    if (error) {
+                        file.error(error);
+                    }
+                    done();
+                });
+            }
+        }, deleteDir);
     }
 
     /**
@@ -306,6 +332,7 @@ export class FileList extends EventEmitter {
      */
     src(...patterns: Pattern[]) {
         return this.pipe({
+            load: false,
             transform: (srcList, destList, patterns) => {
                 const matcher = new Matcher(patterns);
                 srcList.on("data", file => {
@@ -315,7 +342,7 @@ export class FileList extends EventEmitter {
                 });
                 srcList.on("end", () => destList.end());
             }
-        }, patterns, false);
+        }, patterns);
     }
 
     /**
@@ -351,18 +378,53 @@ export class FileList extends EventEmitter {
 }
 
 /**
- * 表示一个处理器。
- * @param file 要处理的文件。
- * @param options 传递给处理器的只读选项。
- * @param done 如果是异步操作，则用于指示异步操作完成的回调函数。
- * @param srcList 源文件列表。
- * @param destList 目标文件列表。
+ * 表示一个简单处理器。
  */
-export type Processor<T> = string | FileList | (((file: File, options: T, done?: () => void, srcList?: FileList, destList?: FileList) => void) | {
-    transform(srcList: FileList, destList: FileList, options: T): void;
-}) & {
-        load?: boolean;
-    };
+export type SimpleProcessor<T> = (file: File, options: T, done?: () => void, srcList?: FileList, destList?: FileList) => void;
+
+/**
+ * 表示一个处理器。
+ */
+export interface Processor<T> {
+
+    /**
+     * 获取当前处理器的名字。
+     */
+    name?: string;
+
+    /**
+     * 判断执行当前处理器前是否需要首先载入文件内容。
+     */
+    load?: boolean;
+
+    /**
+     * 异步处理每个文件。
+     * @param file 要处理的文件。
+     * @param options 传递给处理器的只读选项。
+     * @param done 指示异步操作完成的回调函数。
+     * @param srcList 源文件列表。
+     * @param destList 目标文件列表。
+     */
+    each?(file: File, options?: T, done?: () => void, srcList?: FileList, destList?: FileList): void;
+
+    /**
+     * 处理每个文件。
+     * @param file 要处理的文件。
+     * @param options 传递给处理器的只读选项。
+     * @param srcList 源文件列表。
+     * @param destList 目标文件列表。
+     */
+    eachSync?(file: File, options?: T, srcList?: FileList, destList?: FileList): void;
+
+    /**
+     * 自定义列表转换的逻辑。
+     * @param srcList 源文件列表。
+     * @param destList 目标文件列表。
+     * @param options 传递给处理器的只读选项。
+     */
+    transform?(srcList: FileList, destList: FileList, options?: T): void;
+
+}
 
 /**
  * 默认配置对象。
