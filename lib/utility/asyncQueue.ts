8180a021b73f3d4c5351237dad319a2108985987
xuld <xuld@vip.qq.com>
2016-10-25 11:47:06 +0800

    重构完成：AsyncQueue

--- lib/utility/asyncQueue.ts
+++ lib/utility/asyncQueue.ts
@@ -1,16 +1,32 @@
 /**
  * @fileOverview 异步队列
- * @author xuld<xuld@vip.qq.com>
+ * @author xuld <xuld@vip.qq.com>
  */
 import { Queue } from "./queue";
 
 /**
- * 表示一个异步队列。
+ * 表示一个异步回调函数。
+ * @param done 如果函数声明了 done 形参，则表示当前函数是异步的，函数应在异步任务完成后主动调用 done。
+ * @returns 如果函数返回一个确认对象(Promise)，则队列会自动等待此对象。
  */
-export class AsyncQueue extends Queue<(done?: () => void) => (Promise<any> | void)> {
+export type AsyncCallback = (done?: () => void) => (Promise<any> | void);
+
+/**
+ * 表示一个异步队列。
+ * @remark
+ * 异步队列可以缓冲多个同步或异步的函数，让这些函数按顺序执行。
+ * 队列支持锁定/解锁模型，加锁时，所以等待的异步函数都会等待解锁。
+ * @example
+ * var q = new AsyncQueue();
+ * q.enquee(() => { });
+ * q.enquee(done => { setTimeout(done, 1000); });
+ * q.enquee(() => new Promise(resolve => setTimeout(resolve, 1000)));
+ * q.enquee(() => { q.lock(); setTimeout(() => q.unlock(), 1000); });
+ */
+export class AsyncQueue extends Queue<AsyncCallback> {
 
     /**
-     * 初始化新的队列。
+     * 初始化新的异步队列。
      */
     constructor() {
         super();
@@ -20,38 +36,29 @@ export class AsyncQueue extends Queue<(done?: () => void) => (Promise<any> | voi
     /**
      * 存储当前锁定的次数。
      */
-    private lockCount = 0;
+    private _lock = 0;
 
     /**
-     * 锁定当前队列直到解锁后才继续执行已添加的函数。
+     * 锁定当前队列。锁定后所有函数都将进入等待状态。
      */
     lock() {
-        this.lockCount++;
+        this._lock++;
     }
 
     /**
-     * 解锁当前队列以执行后续回调函数。
+     * 解锁当前队列。解锁后所有函数恢复执行。
      */
     unlock() {
-        this.lockCount--;
+        this._lock--;
+        console.assert(this._lock >= 0, "AsyncQueue: mismatched lock() & unlock() call");
         this.dequeue();
     }
 
     /**
-     * 创建和当前队列等价的确认对象。
-     * @returns 返回一个确认对象。
+     * 在当前队列末尾添加一个回调函数。
+     * @param callback 要添加的回调函数。
      */
-    promise() {
-        return new Promise(resolve => {
-            this.enqueue(resolve);
-        });
-    }
-
-    /**
-     * 在当前队列末尾添加一个异步任务。
-     * @param callback 要添加的任务。
-     */
-    enqueue(callback: (done?: () => void) => (Promise<any> | void)) {
+    enqueue(callback: AsyncCallback) {
         const first = this.empty;
         super.enqueue(callback);
         if (first) {
@@ -60,15 +67,13 @@ export class AsyncQueue extends Queue<(done?: () => void) => (Promise<any> | voi
     }
 
     /**
-     * 从当前队列顶部取出一项。
+     * 从当前队列顶部取出一个回调函数并执行。
+     * @returns 返回取出的回调函数。如果队列为空或已锁定则返回 undefined。
      */
     dequeue() {
-
-        // 如果已加锁，则等待解锁时继续。
-        if (this.lockCount > 0) {
+        if (this._lock > 0) {
             return;
         }
-
         const item = super.dequeue();
         if (item) {
             this.lock();
@@ -84,6 +89,14 @@ export class AsyncQueue extends Queue<(done?: () => void) => (Promise<any> | voi
             }
         }
         return item;
+    }
+
+    /**
+     * 创建和当前队列等价的确认对象(Promise)。
+     * @returns 返回一个确认对象(Promise)。
+     */
+    promise() {
+        return new Promise(resolve => this.enqueue(resolve));
     }
 
 }
--- test/utility/asyncQueueTest.ts
+++ test/utility/asyncQueueTest.ts
@@ -3,7 +3,31 @@ import * as asyncQueue from "../../lib/utility/asyncQueue";
 
 export namespace asyncQueueTest {
 
-    export function thenTest(done: MochaDone) {
+    export function lockTest(done: MochaDone) {
+        let c = 0;
+        const q = new asyncQueue.AsyncQueue();
+        q.lock();
+        setTimeout(() => {
+            q.unlock();
+        }, 5);
+        q.enqueue(done => {
+            assert.equal(++c, 1);
+            setTimeout(done, 7);
+        });
+        q.enqueue(() => {
+            assert.equal(++c, 2);
+        });
+        q.enqueue(() => {
+            assert.equal(++c, 3);
+            done();
+        });
+        q.lock();
+        setTimeout(() => {
+            q.unlock();
+        }, 8);
+    }
+
+    export function enqueueTest(done: MochaDone) {
         let c = 0;
         const q = new asyncQueue.AsyncQueue();
         q.enqueue(done => {
@@ -13,6 +37,7 @@ export namespace asyncQueueTest {
         q.enqueue(() => {
             assert.equal(++c, 2);
         });
+        q.enqueue(() => new Promise(resolver => setTimeout(resolver, 5)));
         q.enqueue(() => {
             assert.equal(++c, 3);
             done();
