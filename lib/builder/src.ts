50faa8245066446bd513eed6311090c7cd64398c
xuld <xuld@vip.qq.com>
2016-10-25 17:59:45 +0800

    增加禁止 src 应用过滤器的功能

--- lib/builder/src.ts
+++ lib/builder/src.ts
@@ -34,7 +34,7 @@ function clearCache() {
  * @param patterns 用于筛选文件的通配符、正则表达式、函数或以上组合的数组。
  * @returns 返回一个文件列表对象。
  */
-export function src(...patterns: Pattern[]) {
+export function src(...patterns: (Pattern | { cwd?: string; base?: string; filter?: boolean })[]) {
     // 遍历匹配 patterns 的文件要注意：
     // 1. 如果设置了 matchedFiles，则直接从 matchedFiles 找出匹配的文件。
     // 2. 应用全局的 matcher 配置。
@@ -43,8 +43,29 @@ export function src(...patterns: Pattern[]) {
     // 5. patterns 可用于决定所有文件发布后的基路径。如 ["src/*.jpg", "src/*.png"] 的基路径是 "src"。
 
     const result = new FileList(asyncQueue);
-    const currentMatcher = new Matcher(patterns);
-    const base = currentMatcher.base;
+
+    let cwd: string;
+    let base: string;
+    let filter: boolean;
+    for (let i = patterns.length - 1; i >= 0; i--) {
+        const pattern = patterns[i];
+        if (typeof pattern === "object" && !Array.isArray(pattern) && !(pattern instanceof RegExp) && !(pattern instanceof Matcher)) {
+            if (pattern.cwd != undefined) {
+                cwd = pattern.cwd;
+            }
+            if (pattern.base != undefined) {
+                base = pattern.base;
+            }
+            if (pattern.filter != undefined) {
+                filter = pattern.filter;
+            }
+            patterns.splice(i, 1);
+        }
+    }
+    const currentMatcher = new Matcher(patterns, cwd);
+    if (base == undefined) {
+        base = currentMatcher.base;
+    }
 
     function add(path: string) {
         result.add(new File(path, pathEquals(path, base) ? getDir(base) : base));
@@ -69,7 +90,7 @@ export function src(...patterns: Pattern[]) {
         glob(currentMatcher, {
             statsCache,
             entriesCache,
-            globalMatcher: matcher,
+            globalMatcher: filter === false ? new Matcher().addIgnore(matcher) : matcher,
             error(error) {
                 verbose(error);
             },
