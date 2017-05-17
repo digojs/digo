f84508bcd4ad17ff37794a9e4f91bccd1f75c328
xuld <xuld@vip.qq.com>
2017-05-17 18:37:52 +0800

    加密

---
+++ lib/utility/crypto.ts
@@ -0,0 +1,25 @@
+import * as crypto from "crypto";
+
+/**
+ * 计算指定数据的 SHA1 值。
+ * @param data 要处理的字符串或缓存对象。
+ * @return 返回结果字符串。其中只包含小写十六进制数值。
+ * @example sha1("A") // "6dcd4ce23d88e2ee9568ba546c007c63d9131c1b"
+ */
+export function sha1(data: string | Buffer) {
+    const hash = crypto.createHash("sha1");
+    hash.update(data);
+    return hash.digest("hex");
+}
+
+/**
+ * 计算指定数据的 MD5 值。
+ * @param data 要处理的字符串或缓存对象。
+ * @return 返回结果字符串。其中只包含小写十六进制数值。
+ * @example md5("A") // "7fc56270e7a70fa81a5935b72eacbe29"
+ */
+export function md5(data: string | Buffer) {
+    const hash = crypto.createHash("md5");
+    hash.update(data);
+    return hash.digest("hex");
+}
---
+++ test/utility/cryptoTest.ts
@@ -0,0 +1,14 @@
+import * as assert from "assert";
+import * as crypto from "../../lib/utility/crypto";
+
+export namespace cryptoTest {
+
+    export function sha1Test() {
+        assert.equal(crypto.sha1("foo"), "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33");
+    }
+
+    export function md5Test() {
+        assert.equal(crypto.md5("foo"), "acbd18db4cc2f85cedef654fccc4a4d8");
+    }
+
+}
